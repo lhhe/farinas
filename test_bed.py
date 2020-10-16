19bda035fafabdd9d8e01d44c1748861db9c84ce
Leo <hernandez.leo@gmail.com>
2020-10-16 16:16:28 -0700

    Updated example

--- test_bed.py
+++ test_bed.py
@@ -1,13 +1,20 @@
 import numpy as np
 
+# Create a test vector
 test = np.array([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0])
 print(len(test))
+# Find indexes of 1s
+
 location = np.where(test == 1)[0]
 print(location)
 
+# Fistst 1
 inicio = location.min()
+# Last 1
 fin = location.max()
-print(inicio)
-print(fin)
-print("##")
-print(np.unique(test, return_index=True))
+
+print(f"the first 1 occurs at index {inicio}")
+print(f"the las one occurs at index {fin}")
+
+# How many groups of 1s?
+
